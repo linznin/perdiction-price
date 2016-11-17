1feefbbf3cd66fe9e542f4c56a6cac48a1a68700
Linznin <linznin@gmail.com>
2016-11-17 23:04:35 +0800

    set config

--- src/org/gandhim/pso/PSOConstants.java
+++ src/org/gandhim/pso/PSOConstants.java
@@ -6,11 +6,11 @@ package org.gandhim.pso;
 // you can modify the value depends on your needs
 
 public interface PSOConstants {
-	int SWARM_SIZE = 2;
+	int SWARM_SIZE = 10;
 	int MAX_ITERATION = 10;
 	int PROBLEM_DIMENSION = 1;
-	double C1 = 2.5;
-	double C2 = 2.5;
+	double C1 = 2;
+	double C2 = 2;
 	double W_UPPERBOUND = 1.0;
-	double W_LOWERBOUND = 0.5;
+	double W_LOWERBOUND = 0.8;
 }
