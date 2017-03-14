1a3d116dbccccc3e908ce6b2148316e91df8008e
Linznin <linznin@gmail.com>
2017-03-14 10:36:21 +0800

    財務字典搜尋字

--- src/org/text/core/FileConstants.java
+++ src/org/text/core/FileConstants.java
@@ -4,11 +4,13 @@ package org.text.core;
  * Created by linznin on 2017/2/17.
  */
 public interface FileConstants {
-    String dicPath = "/Users/linznin/tmp/dic/cliwc_v1.txt";
+    String dicPath = "/Users/linznin/tmp/dic/dictionary";
+    String dicFile = "/Users/linznin/tmp/dic/cliwc_v1.txt";
     String textPath = "/Users/linznin/tmp/Keywords";
     String newsPath = "/Users/linznin/tmp/news";
     String newsSeqPath = "/Users/linznin/tmp/newsFeater";
     String resultPath = "/Users/linznin/tmp/csv/csvtest.csv";
 
     String[] targetFeatrue = {"posemo", "negemo", "anx", "sad", "cogmech", "affect"};
+    String[] searchFile = {"ModalStrong.txt","ModalWeak.txt","Negative.txt","Postive.txt","Uncertainty.txt"};
 }
