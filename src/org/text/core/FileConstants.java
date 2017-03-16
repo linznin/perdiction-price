50f54d71980383b914d28cc6faf98c3d54feff52
Linznin <linznin@gmail.com>
2017-03-16 10:29:03 +0800

    增加搜尋生氣詞彙

--- src/org/text/core/FileConstants.java
+++ src/org/text/core/FileConstants.java
@@ -7,10 +7,10 @@ public interface FileConstants {
     String dicPath = "/Users/linznin/tmp/dic/dictionary";
     String dicFile = "/Users/linznin/tmp/dic/cliwc_v1.txt";
     String textPath = "/Users/linznin/tmp/Keywords";
-    String newsPath = "/Users/linznin/tmp/news";
+    String newsPath = "/Users/linznin/tmp/News";
     String newsSeqPath = "/Users/linznin/tmp/newsFeater";
-    String resultPath = "/Users/linznin/tmp/csv/csvtest.csv";
+    String resultPath = "/Users/linznin/tmp/csv/csvtest111.csv";
 
-    String[] targetFeatrue = {"posemo", "negemo", "anx", "sad", "cogmech", "affect"};
+    String[] targetFeatrue = {"posemo", "negemo", "anx", "sad", "cogmech", "affect", "anger"};
     String[] searchFile = {"ModalStrong.txt","ModalWeak.txt","Negative.txt","Postive.txt","Uncertainty.txt"};
 }
