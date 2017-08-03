ad1b25dd6e2b1cb905735668b2ab76a2a28912b0
Linznin <linznin@gmail.com>
2017-08-03 13:34:28 +0800

    remove unused code

--- src/org/text/core/TextConstants.java
+++
@@ -1,9 +0,0 @@
-package org.text.core;
-
-/**
- * Created by linznin on 2017/2/17.
- */
-public interface TextConstants {
-    String[] targetFeatrue = {"posemo", "negemo", "anx", "sad", "cogmech", "affect", "anger"};
-    String[] searchFile = {"ModalStrong.txt","ModalWeak.txt","Negative.txt","Postive.txt","Uncertainty.txt"};
-}
